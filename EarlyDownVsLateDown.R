ea227394a8210817f9dd6de5cd1c4b2a943d0a5d
Shaan Kalgaonkar <95394067+shaankal@users.noreply.github.com>
2025-07-15 16:16:03 -0700

    Created Metrics

--- EarlyDownVsLateDown.R
+++ EarlyDownVsLateDown.R
@@ -14,8 +14,19 @@ pbp <- pbp %>%
   mutate(
     down_bucket = case_when(
       down == 1 & ydstogo == 10 ~ "1st & 10",
-      down %in% c(3,4) ~ "3rd/4th Down",
+      down %in% c(3, 4) ~ "3rd/4th Down",
       TRUE ~ NA_character_
-    )
+    ),
+    shotgun_type = ifelse(shotgun == 1, "Shotgun", "Under Center"),
+    conversion = if_else(down %in% c(3, 4) & first_down == 1, 1, 0)
   ) %>%
   filter(!is.na(down_bucket))
+
+
+
+pbp <- pbp %>%
+  mutate(
+    shotgun_type = ifelse(shotgun == 1, "Shotgun", "Under Center"),
+    play_action_type = ifelse(play_action == 1, "Play Action", "Standard Dropback"),
+    conversion = if_else(down %in% c(3, 4) & first_down == 1, 1, 0)
+  )
